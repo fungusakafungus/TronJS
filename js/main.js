e98594929e60287c054ed7826c5455ac651d1968
Ilya Margolin <ilya@jimdo.com>
2012-02-12 15:19:30 +0100

    main js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 // 3 event sources: keyboard events, timer events, websocket events
 // on keyboard and websocket: store movements in an array
 // on timer event: process movements and update canvas
- $(document).ready(function() {
-   
- });
+$(document).ready(function() {
+    var movements = [];
+    function updateField(){
+        $.each(players,function(player) {
+            var now = new Date().getTime();
+            var timediff = player.updated - now;
+            // ???
+        });
+        while(movements) {
+            var movement = movements.pop();
+            // ???
+        }
+    }
+    setInterval(updateField, 10);
+
+});
