535ddc4dfe0a2b765ae0bf3f2eca303caad507df
Ilya Margolin <ilya@jimdo.com>
2012-02-12 14:43:48 +0100

    main.js comments

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// 3 event sources: keyboard events, timer events, websocket events
+// on keyboard and websocket: store movements in an array
+// on timer event: process movements and update canvas
