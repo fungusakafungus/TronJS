385815bd962f44f30da85b20b23ce074e5055edb
Ilya Margolin <ilya@jimdo.com>
2012-02-12 17:50:31 +0100

    whitespace plus trail

--- js/Player.js
+++ js/Player.js
@@ -1,5 +1,5 @@
 function Player() {
-   
+
 }
 
 Player.prototype = {
@@ -13,35 +13,33 @@ Player.prototype = {
         x: 10,
         y: 10
     },
-    
-  
+
     bindKeys: function() {
         var self = this;
         $(document).bind('keydown', 'left right', function(event) {
             self.addEvent(event)
         });
     },
-  
+
     addEvent: function(event) {
         console.log(this);
         this.events.push(event);
     },
-  
+
     updatePosition: function() {
-        var now = new Date().getTime(),
-        timediff = now - this.updated;
-        
-        while(this.events.length > 0){
+        var now = new Date().getTime(), timediff = now - this.updated;
+
+        if (this.events.length > 0) {
             var event = this.events.pop();
             this.setDirection(event.keyCode);
         }
-            
+
         this.position.x += this.speed.x * timediff;
         this.position.y += this.speed.y * timediff;
-        
+
         this.updated = now;
     },
-    
+
     setDirection: function(direction) {
         var new_x, new_y;
         switch (direction) {
@@ -50,12 +48,13 @@ Player.prototype = {
                 new_y = - this.speed.x;
                 break;
             case 39:
-                new_x  = - this.speed.y;
+                new_x = - this.speed.y;
                 new_y = this.speed.x;
                 break
-        };
+        }
+
         this.speed.x = new_x;
         this.speed.y = new_y;
     }
- 
+
 };
--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
 // 3 event sources: keyboard events, timer events, websocket events
 // on keyboard and websocket: store movements in an array
 // on timer event: process movements and update canvas
- $(document).ready(function() {
-     player = new Player();
-     player.bindKeys();
+$(document).ready(function() {
+    player = new Player();
+    player.bindKeys();
     var movements = [];
     var $field = $('#field');
-    
+
     function updateField() {
         $player = $('#one');
-        
-        if($player.position().left <= 0 || $player.position().top <= 0 || 
+
+        if ($player.position().left <= 0 || $player.position().top <= 0 ||
             $player.position().left >= $field.width() - $player.width() ||
             $player.position().top >= $field.height() - $player.height()
-    ) {
-            
+            ) {
+
             player.speed.x = 0;
-            player.speed.y = 0; 
+            player.speed.y = 0;
         }
-        
+
         player.updatePosition();
+        $player.clone().appendTo($field);
         $player.css({
             left: player.position.x,
             top: player.position.y
         });
     }
 
-    setInterval(updateField, 0);
+    setInterval(updateField, 5);
 
 });
