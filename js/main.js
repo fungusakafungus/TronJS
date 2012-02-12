1b6dc9da7325204dae4ab4ed28cd0baf494661ea
Ilya Margolin <ilya@jimdo.com>
2012-02-12 14:56:56 +0100

    wip

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 // 3 event sources: keyboard events, timer events, websocket events
 // on keyboard and websocket: store movements in an array
 // on timer event: process movements and update canvas
+ $(document).ready(function() {
+   $("a").click(function() {
+     alert("Hello world!");
+   });
+ });
